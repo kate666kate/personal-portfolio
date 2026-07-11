14cbc8f81d304b900f232d3424c756c2daa10e6f
kate666kate <70002450+kate666kate@users.noreply.github.com>
2026-07-11 17:05:40 +1000

    Enhance project cards with outcomes and tags

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,6 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, outcome, tags = [], imgUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
@@ -8,6 +8,10 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
+          <p>{outcome}</p>
+          <div className="project-tags">
+            {tags.map((tag) => <small key={tag}>{tag}</small>)}
+          </div>
         </div>
       </div>
     </Col>
