a16799795ed0e725aaf503dd6c35f800c040382c
kate666kate <70002450+kate666kate@users.noreply.github.com>
2026-07-11 18:19:00 +1000

    Update footer branding and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
@@ -12,15 +11,14 @@ export const Footer = () => {
         <Row className="align-items-center">
           <MailchimpForm />
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+            <img src={logo} alt="Meisi Xu Digital Growth" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://github.com/kate666kate" aria-label="GitHub profile"><img src={navIcon1} alt="" /></a>
+              <a href="mailto:katexu2026@gmail.com" aria-label="Email Meisi"><img src={navIcon3} alt="" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Meisi Xu - Digital Growth Portfolio</p>
           </Col>
         </Row>
       </Container>
