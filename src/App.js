391865c65c06feec261a4eff5555b7723b53b172
kate666kate <70002450+kate666kate@users.noreply.github.com>
2026-07-12 12:57:17 +1000

    Add plugin stack section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { RoleFit } from "./components/RoleFit";
 import { WorkingFlow } from "./components/WorkingFlow";
+import { PluginStack } from "./components/PluginStack";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
@@ -17,6 +18,7 @@ function App() {
       <Skills />
       <RoleFit />
       <WorkingFlow />
+      <PluginStack />
       <Projects />
       <Contact />
       <Footer />
